8d69a532854a7c96367edfdf59a4aa81047041a4
MacBox7 <ajankit2304@gmail.com>
2016-11-12 00:43:49 +0530

    config_structure added

---
+++ config_structure.js
@@ -0,0 +1,12 @@
+/**
+*  strcuture of the config.js file
+   rename this file to config.js
+*/
+
+module.exports = {
+	'host' : '',
+	'username' : '',
+	'password' : '',
+	'database' : '',
+	'charset' : ''
+}
--- models/events.js
+++ models/events.js
@@ -3,23 +3,64 @@
 module.exports = function(sequelize, DataTypes) {
   var events = sequelize.define("events", {        //Must be same as table name
     id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true // Automatically gets converted to SERIAL for postgres
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true // Automatically gets converted to SERIAL for postgres
     },
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    venue: DataTypes.STRING,
-    startTime: DataTypes.STRING,
-    endTime: DataTypes.STRING,
-    startDate: DataTypes.STRING,
-    endDate: DataTypes.STRING,
-    currentRound: DataTypes.STRING,
-    society: DataTypes.STRING,
-    category: DataTypes.STRING,
-    maxContestants: DataTypes.INTEGER,
-    status: DataTypes.STRING,
-    pdf: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      notNull: true
+    },
+    description: {
+      type: DataTypes.STRING,
+      notNull: true
+    },
+    venue: {
+      type: DataTypes.STRING,
+      notNull: true
+    },
+    startTime: {
+      type: DataTypes.STRING,
+      notNull: true
+    },
+    endTime: {
+      type: DataTypes.STRING,
+      notNull: true
+    },
+    startDate: {
+      type: DataTypes.STRING,
+      notNull: true
+    },
+    endDate: {
+      type: DataTypes.STRING,
+      notNull: true
+    },
+    currentRound: {
+      type: DataTypes.STRING,
+      notNull: true
+    },
+    society: {
+      type: DataTypes.STRING,
+      notNull: true
+    }
+    category: {
+      type: DataTypes.STRING,
+      notNull: true
+    },
+    maxContestants: {
+      type: DataTypes.INTEGER,
+      notNull: true
+    },
+    status: {
+      type: DataTypes.STRING,
+      notNull: true
+    },
+    pdf: {
+      type: DataTypes.STRING,
+      notNull: true
+    }
+  }, {
+    timestamps: false
   });
   return events;
 };
